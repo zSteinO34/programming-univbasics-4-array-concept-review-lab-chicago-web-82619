1555173c6820e1f2a947301a18f1b6a4d99f3052
Zac Steinberg <zstein1234@gmail.com>
2019-08-13 14:59:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,12 @@ end
 
 def find_max_value(array)
   max = 0
-  array.length.times
+  array.length.times { |i|
+    if array[i] > max
+      max = array[i]      
+    end
+  }
+  return max
 end
 
 def find_min_value(array)
